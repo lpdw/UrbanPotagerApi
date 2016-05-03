2208f8ffb4b3d3634f60b0a9479f8615d6c069eb
Eraac <kevin@labesse.me>
2016-05-03 22:26:31 +0200

    add find by slug

--- src/CoreBundle/Repository/TypeRepository.php
+++ src/CoreBundle/Repository/TypeRepository.php
@@ -10,4 +10,12 @@ namespace CoreBundle\Repository;
  */
 class TypeRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function findBySlug($slug)
+    {
+        $qb = $this->createQueryBuilder('t')
+                    ->where('t.slug = :slug')
+                    ->setParameter('slug', $slug);
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
 }
