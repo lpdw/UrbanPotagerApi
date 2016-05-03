49e527c2f9a46434281dd722386768a758cc6264
Eraac <kevin@labesse.me>
2016-05-03 23:28:27 +0200

    CRUD Type

---
+++ src/CoreBundle/Controller/TypeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace CoreBundle\Controller;
+
+use CoreBundle\Security\TypeVoter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use CoreBundle\Entity\Type;
+use CoreBundle\Form\Type\TypeType;
+
+class TypeController extends CoreController
+{
+    // TODO add filter
+    public function getTypesAction()
+    {
+        /** @var \CoreBundle\Repository\TypeRepository $repo */
+        $repo = $this->getRepository();
+
+        $types = $repo->findAll();
+
+        return [
+            'count' => count($types),
+            'types' => $types,
+        ];
+    }
+
+    // TODO param converter
+    public function getTypeAction(Type $type)
+    {
+        $this->isGranted(TypeVoter::VIEW, $type);
+
+        return [
+            'type' => $type,
+        ];
+    }
+
+    public function postTypeAction(Request $request)
+    {
+        $this->isGranted(TypeVoter::CREATE, $type = new Type());
+
+        return $this->formType($type, $request, 'post');
+    }
+
+    public function patchTypeAction(Type $type, Request $request)
+    {
+        $this->isGranted(TypeVoter::EDIT, $type);
+
+        return $this->formType($type, $request, 'patch'); // TODO form for edit (can not change name ?)
+    }
+
+    public function deleteTypeAction(Type $type)
+    {
+        $this->isGranted(TypeVoter::DELETE, $type);
+
+        $this->getManager()->remove($type);
+        $this->getManager()->flush();
+
+        return new JsonResponse([], 204);
+    }
+
+    private function formType(Type $type, Request $request, $method = 'post')
+    {
+        $form = $this->createForm(TypeType::class, $type, ['method' => $method]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getManager();
+            $em->persist($type);
+            $em->flush();
+
+            return [
+                'type' => $type,
+            ];
+        }
+
+        return new JsonResponse($this->getAllErrors($form), 400);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRepositoryName()
+    {
+        return 'CoreBundle:Type';
+    }
+}
---
+++ src/CoreBundle/Form/Type/TypeType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use CoreBundle\Entity\Type;
+
+class TypeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'plant' => Type::PLANT,
+                    'sensor' => Type::SENSOR,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'CoreBundle\Entity\Type',
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return ''; // TODO change ?
+    }
+}
