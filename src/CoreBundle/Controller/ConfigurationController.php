84c66f5086a89ab85f9cde6da58a10285a56af73
Eraac <kevin@labesse.me>
2016-05-04 22:40:11 +0200

    add form configuration

---
+++ src/CoreBundle/Controller/ConfigurationController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace CoreBundle\Controller;
+
+use CoreBundle\Security\ConfigurationVoter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use CoreBundle\Entity\Configuration;
+use CoreBundle\Form\Type\ConfigurationType;
+
+class ConfigurationController extends CoreController
+{
+    // TODO add filter
+    /**
+     * @View(serializerGroups={"Default"})
+     */
+    public function getConfigurationsAction()
+    {
+        /** @var \CoreBundle\Repository\ConfigurationRepository $repo */
+        $repo = $this->getRepository();
+
+        $configurations = $repo->findAll();
+
+        return [
+            'count' => count($configurations),
+            'configurations' => $configurations,
+        ];
+    }
+
+    /**
+     * @View(serializerGroups={"Default", "detail-configuration"})
+     * @ParamConverter("configuration", options={"mapping": {"configuration": "slug"}})
+     */
+    public function getConfigurationAction(Configuration $configuration)
+    {
+        $this->isGranted(ConfigurationVoter::VIEW, $configuration);
+
+        return [
+            'configuration' => $configuration,
+        ];
+    }
+
+    /**
+     * @View(serializerGroups={"Default", "detail-configuration"})
+     */
+    public function postConfigurationAction(Request $request)
+    {
+        $this->isGranted(ConfigurationVoter::CREATE, $configuration = new Configuration());
+
+        $configuration->setOwner($this->getUser());
+
+        return $this->formType($configuration, $request, 'post');
+    }
+
+    /**
+     * @View(serializerGroups={"Default", "detail-configuration"})
+     * @ParamConverter("configuration", options={"mapping": {"configuration": "slug"}})
+     */
+    public function patchConfigurationAction(Configuration $configuration, Request $request)
+    {
+        $this->isGranted(ConfigurationVoter::EDIT, $configuration);
+
+        return $this->formType($configuration, $request, 'patch');
+    }
+
+    /**
+     * @ParamConverter("configuration", options={"mapping": {"configuration": "slug"}})
+     */
+    public function deleteConfigurationAction(Configuration $configuration)
+    {
+        $this->isGranted(ConfigurationVoter::DELETE, $configuration);
+
+        $this->getManager()->remove($configuration);
+        $this->getManager()->flush();
+
+        return new JsonResponse([], self::NO_CONTENT);
+    }
+
+    private function formType(Configuration $configuration, Request $request, $method = 'post')
+    {
+        $form = $this->createForm(ConfigurationType::class, $configuration, ['method' => $method]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getManager();
+            $em->persist($configuration);
+            $em->flush();
+
+            return new JsonResponse([
+                    'configuration' => $configuration,
+                ],
+                'post' === $method ? self::CREATED : self::OK
+            );
+        }
+
+        return new JsonResponse($this->getAllErrors($form), self::BAD_REQUEST);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRepositoryName()
+    {
+        return 'CoreBundle:Configuration';
+    }
+}
