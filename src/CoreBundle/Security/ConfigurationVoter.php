4148ef998271a04cb095b5fa741d24852d0b154c
Eraac <kevin@labesse.me>
2016-05-04 21:21:42 +0200

    add configuration voter

---
+++ src/CoreBundle/Security/ConfigurationVoter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace CoreBundle\Security;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use CoreBundle\Entity\Configuration;
+use UserBundle\Entity\User;
+
+class ConfigurationVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        return parent::supports($attribute, $subject) && $subject instanceof Configuration;
+    }
+
+    /**
+     * @param Configuration $entity
+     * @param TokenInterface $token
+     *
+     * @return boolean
+     */
+    protected function canView($entity, TokenInterface $token)
+    {
+        return $this->isOwner($entity, $this->getUser($token));
+    }
+
+    /**
+     * @param Configuration $entity
+     * @param TokenInterface $token
+     *
+     * @return boolean
+     */
+    protected function canCreate($entity, TokenInterface $token)
+    {
+        return $this->isConnected($token);
+    }
+
+    /**
+     * @param Configuration $entity
+     * @param TokenInterface $token
+     *
+     * @return boolean
+     */
+    protected function canEdit($entity, TokenInterface $token)
+    {
+        return $this->isOwner($entity, $this->getUser($token));
+    }
+
+    /**
+     * @param Configuration $entity
+     * @param TokenInterface $token
+     *
+     * @return boolean
+     */
+    protected function canDelete($entity, TokenInterface $token)
+    {
+        return $this->canEdit($entity, $token);
+    }
+
+    private function isOwner(Configuration $configuration, User $user)
+    {
+        $owner = $configuration->getOwner();
+
+        if (is_null($owner) || is_null($user)) {
+            return false;
+        }
+
+        return $owner->getId() === $user->getId();
+    }
+}
