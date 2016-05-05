1927e82927f609601480af54c9e0f7d72d19eaca
Eraac <kevin@labesse.me>
2016-05-05 20:06:51 +0200

    re-indent code

--- src/CoreBundle/Controller/GardenController.php
+++ src/CoreBundle/Controller/GardenController.php
@@ -4,54 +4,47 @@ namespace CoreBundle\Controller;
 
 use CoreBundle\Entity\Garden;
 use CoreBundle\Form\Type\GardenType;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-
 class GardenController extends CoreController
 {
-  public function getGardensAction()
-  {
-    $repo = $this->getRepository();
+    public function getGardensAction()
+    {
+        $repo = $this->getRepository();
 
-    $gardens = $repo->findAll();
-    return [
-        'count' => count($gardens),
-        'gardens' => $gardens,
-    ];
-  } // "get_gardens"            [GET] /gardens
+        $gardens = $repo->findAll();
 
-  public function postGardensAction(Request $request)
-  {
-    return $this->formGarden(new Garden(), $request, "post");
+        return [
+            'count' => count($gardens),
+            'gardens' => $gardens,
+        ];
+    }
 
-  } // "post_gardens"           [POST] /gardens
+    public function postGardensAction(Request $request)
+    {
+        return $this->formGarden(new Garden(), $request, "post");
+    }
 
-  public function getGardenAction($id)
+    public function getGardenAction($id)
     {
 
-    } // "get_garden"            [GET] /gardens/:id
+    }
 
-  private function formGarden(Garden $garden, Request $request, $method='post')
-  {
-    $form = $this->createForm(GardenType::class, $garden, array('method' => $method));
-    $form->handleRequest($request);
+    private function formGarden(Garden $garden, Request $request, $method = 'post')
+    {
+        $form = $this->createForm(GardenType::class, $garden, array('method' => $method));
+        $form->handleRequest($request);
 
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($garden);
+        $em->flush();
 
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($garden);
-      $em->flush();
+        return $garden;
+    }
 
-      return $garden;
-
-  }
-
-  /**
-   * @return string
-   */
-  protected function getRepositoryName()
-  {
-      return 'CoreBundle:Garden';
-  }
+    protected function getRepositoryName()
+    {
+        return 'CoreBundle:Garden';
+    }
 }
--- src/CoreBundle/Form/Type/GardenType.php
+++ src/CoreBundle/Form/Type/GardenType.php
@@ -12,37 +12,39 @@ use CoreBundle\Entity\Garden;
 
 class GardenType extends AbstractType
 {
-  /**
-   * @param FormBuilderInterface $builder
-   * @param array $options
-   */
-  public function buildForm(FormBuilderInterface $builder, array $options)
-  {
-    $builder
-      ->add('name', TextType::class)
-      ->add('description', TextareaType::class)
-      ->add('isPublic', CheckboxType::class)
-      ->add('latitude', TextType::class)
-      ->add('longitude', TextType::class)
-      ->add('show_location', CheckboxType::class)
-      ->add('country', TextType::class)
-      ->add('city', TextType::class)
-      ->add('zip_code', TextType::class)
-      ->add('address1', TextType::class)
-      ->add('address2', TextType::class)
-    ;
-  }
-  /**
-   * @param OptionsResolver $resolver
-   */
-  public function configureOptions(OptionsResolver $resolver)
-  {
-      $resolver->setDefaults([
-          'data_class' => 'CoreBundle\Entity\Garden',
-      ]);
-  }
-  public function getBlockPrefix()
-  {
-      return ''; // TODO change ?
-  }
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('isPublic', CheckboxType::class)
+            ->add('latitude', TextType::class)
+            ->add('longitude', TextType::class)
+            ->add('show_location', CheckboxType::class)
+            ->add('country', TextType::class)
+            ->add('city', TextType::class)
+            ->add('zip_code', TextType::class)
+            ->add('address1', TextType::class)
+            ->add('address2', TextType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'CoreBundle\Entity\Garden',
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return ''; // TODO change ?
+    }
 }
