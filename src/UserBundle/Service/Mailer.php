7d7455820c91910eb50c1dcebbccebcdfd2f2211
Eraac <kevin@labesse.me>
2016-05-14 12:40:00 +0200

    add way to reset forget password

---
+++ src/UserBundle/Service/Mailer.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace UserBundle\Service;
+
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Mailer\MailerInterface;
+
+class Mailer implements MailerInterface
+{
+    /**
+     * @var \Swift_Mailer
+     */
+    protected $mailer;
+
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * @var array
+     */
+    protected $parameters;
+
+    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig, array $parameters)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->parameters = $parameters;
+    }
+
+    public function sendConfirmationEmailMessage(UserInterface $user)
+    {
+        $template = $this->parameters['template']['confirmation'];
+
+        $context = array(
+            'user' => $user,
+            'token' => $user->getConfirmationToken()
+        );
+
+        $this->sendMessage($template, $context, $this->parameters['from_email']['confirmation'], $user->getEmail());
+    }
+
+    public function sendResettingEmailMessage(UserInterface $user)
+    {
+        $template = $this->parameters['template']['resetting'];
+
+        $context = array(
+            'user' => $user,
+            'token' => $user->getConfirmationToken()
+        );
+
+        $this->sendMessage($template, $context, $this->parameters['from_email']['resetting'], $user->getEmail());
+    }
+
+    /**
+     * @param string $templateName
+     * @param array  $context
+     * @param string $fromEmail
+     * @param string $toEmail
+     */
+    protected function sendMessage($templateName, $context, $fromEmail, $toEmail)
+    {
+        $template = $this->twig->loadTemplate($templateName);
+        $subject = $template->renderBlock('subject', $context);
+        $textBody = $template->renderBlock('body_text', $context);
+        $htmlBody = $template->renderBlock('body_html', $context);
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject($subject)
+            ->setFrom($fromEmail)
+            ->setTo($toEmail);
+
+        if (!empty($htmlBody)) {
+            $message->setBody($htmlBody, 'text/html')
+                    ->addPart($textBody, 'text/plain');
+        } else {
+            $message->setBody($textBody);
+        }
+
+        $this->mailer->send($message);
+    }
+}
