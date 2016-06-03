7fb6e20e540206ce4b4561cb53ee6a23b5066ef8
Naliae <magali.heraud@hotmail.fr>
2016-06-03 14:33:26 +0200

    generate Post entity

---
+++ src/CoreBundle/Entity/Post.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table(name="post")
+ * @ORM\Entity(repositoryClass="CoreBundle\Repository\PostRepository")
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="postType", type="string")
+ * @ORM\DiscriminatorMap({
+ *  "Post" = "Post",
+ *  "MeasurePost" = "MeasurePost",
+ *  "UserPost" = "UserPost",
+ * })
+ */
+class Post
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    protected $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255)
+     */
+    protected $message;
+
+    /**
+     * @var garden
+     *
+     * @ORM\ManyToOne(targetEntity="CoreBundle\Entity\Garden")
+     * @ORM\JoinColumn(onDelete="CASCADE")
+     */
+    protected $garden;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Post
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Post
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set garden
+     *
+     * @param \CoreBundle\Entity\Garden $garden
+     *
+     * @return Post
+     */
+    public function setGarden(\CoreBundle\Entity\Garden $garden = null)
+    {
+        $this->garden = $garden;
+
+        return $this;
+    }
+
+    /**
+     * Get garden
+     *
+     * @return \CoreBundle\Entity\Garden
+     */
+    public function getGarden()
+    {
+        return $this->garden;
+    }
+}
