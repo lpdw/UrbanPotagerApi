6c0b795781efc710fd0b128a21459cef373de45a
Eraac <kevin@labesse.me>
2016-06-09 01:06:17 +0200

    add first test

---
+++ tests/CoreBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+}
